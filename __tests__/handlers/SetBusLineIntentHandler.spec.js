8231343f1aba507e8bb0c2964ea3c44e110b1c2a
Deividson Damasio <deividsondamasio@yahoo.com.br>
2020-12-28 17:40:22 -0300

    Fix irrelevant response when invoked SetBusLineIntent

--- __tests__/handlers/SetBusLineIntentHandler.spec.js
+++ __tests__/handlers/SetBusLineIntentHandler.spec.js
@@ -2,12 +2,24 @@ const Alexa = require('ask-sdk-core');
 
 const BusLine = require('../../lambda/responses/BusLineResponse');
 const SetBusLineIntentHandler = require('../../lambda/handlers/SetBusLineIntentHandler');
+const speaks = require('../../lambda/speakStrings');
 
 describe('Sequence 06. Test scenario: SetBusLineIntent', () => {
+  const getSessionAttributes = jest.fn();
+  const setPersistentAttributes = jest.fn();
+  const savePersistentAttributes = jest.fn();
   const getResponse = jest.fn();
+  const mockConsoleError = jest.fn();
+  // eslint-disable-next-line no-console
+  console.error = mockConsoleError;
   BusLine.getResponse = getResponse;
 
   const handlerInput = {
+    attributesManager: {
+      getSessionAttributes,
+      setPersistentAttributes,
+      savePersistentAttributes,
+    },
     requestEnvelope: {
       request: {
         type: 'IntentRequest',
@@ -26,6 +38,7 @@ describe('Sequence 06. Test scenario: SetBusLineIntent', () => {
     },
     responseBuilder: Alexa.ResponseFactory.init(),
   };
+  const testResponseBuilder = Alexa.ResponseFactory.init();
 
   beforeEach(() => {
     handlerInput.requestEnvelope.request.intent.name = 'SetBusLineIntent';
@@ -41,9 +54,58 @@ describe('Sequence 06. Test scenario: SetBusLineIntent', () => {
     expect(SetBusLineIntentHandler.canHandle(handlerInput)).toEqual(true);
   });
 
+  it('should be able can return with response I have a problem when an exception happens', async () => {
+    getSessionAttributes.mockReturnValueOnce({
+      codBusStop: '8711',
+      especificBusLine: true,
+    });
+    getResponse.mockImplementationOnce(() => {
+      throw new Error('InternalError'); // Simula um erro genérico.
+    });
+
+    const outputSpeech = testResponseBuilder
+      .speak(speaks.PROBLEM)
+      .withStandardCard(speaks.SKILL_NAME, speaks.PROBLEM)
+      .withShouldEndSession(true)
+      .getResponse();
+
+    const response = await SetBusLineIntentHandler.handle(handlerInput);
+
+    expect(response).toEqual(outputSpeech);
+    expect(mockConsoleError).toHaveBeenCalledWith(
+      'Error:',
+      'SetBusLineIntentHandler - Error: InternalError',
+    );
+  });
+
+  it('should be able can return response not understand when getSessionAttributes return null', async () => {
+    getSessionAttributes.mockReturnValueOnce(null);
+
+    const outputSpeech = testResponseBuilder
+      .speak(speaks.NOT_UNDERSTAND + speaks.OPTIONS)
+      .withStandardCard(
+        speaks.SKILL_NAME,
+        speaks.NOT_UNDERSTAND + speaks.OPTIONS_CARD,
+      )
+      .reprompt(speaks.OPTIONS)
+      .getResponse();
+
+    const response = await SetBusLineIntentHandler.handle(handlerInput);
+
+    expect(response).toEqual(outputSpeech);
+    expect(mockConsoleError).not.toHaveBeenCalled();
+  });
+
   it('should be able can return response', async () => {
+    getSessionAttributes.mockReturnValueOnce({
+      codBusStop: '8711',
+      especificBusLine: true,
+    });
+
     await SetBusLineIntentHandler.handle(handlerInput);
 
+    // Apenas verifica se chamou o método.
     expect(getResponse).toHaveBeenCalled();
+    expect(mockConsoleError).not.toHaveBeenCalled();
   });
 });
--- lambda/handlers/SetBusLineIntentHandler.js
+++ lambda/handlers/SetBusLineIntentHandler.js
@@ -1,6 +1,8 @@
 const Alexa = require('ask-sdk-core');
 
 const BusLine = require('../responses/BusLineResponse');
+const speaks = require('../speakStrings');
+const { setLastAccess } = require('../util');
 
 const SetBusLineIntentHandler = {
   canHandle(handlerInput) {
@@ -10,8 +12,49 @@ const SetBusLineIntentHandler = {
     );
   },
   async handle(handlerInput) {
-    const response = await BusLine.getResponse(handlerInput);
-    return response;
+    try {
+      const { attributesManager } = handlerInput;
+      const sessionAttributes = attributesManager.getSessionAttributes() || {};
+
+      const codBusStop = Object.prototype.hasOwnProperty.call(
+        sessionAttributes,
+        'codBusStop',
+      )
+        ? sessionAttributes.codBusStop
+        : false;
+
+      const especificBusLine = Object.prototype.hasOwnProperty.call(
+        sessionAttributes,
+        'especificBusLine',
+      )
+        ? sessionAttributes.especificBusLine
+        : false;
+
+      if (codBusStop !== false && especificBusLine !== false) {
+        const response = await BusLine.getResponse(handlerInput);
+        return response;
+      }
+
+      return handlerInput.responseBuilder
+        .speak(speaks.NOT_UNDERSTAND + speaks.OPTIONS)
+        .withStandardCard(
+          speaks.SKILL_NAME,
+          speaks.NOT_UNDERSTAND + speaks.OPTIONS_CARD,
+        )
+        .reprompt(speaks.OPTIONS)
+        .getResponse();
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error('Error:', `SetBusLineIntentHandler - ${error}`);
+    }
+
+    await setLastAccess(handlerInput);
+
+    return handlerInput.responseBuilder
+      .speak(speaks.PROBLEM)
+      .withStandardCard(speaks.SKILL_NAME, speaks.PROBLEM)
+      .withShouldEndSession(true)
+      .getResponse();
   },
 };
 
